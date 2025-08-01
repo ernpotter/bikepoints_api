8748c1d1a5678a1b8e7ac0b5bad256686afff802
ErinPotter <erinpotte@gmail.com>
2025-08-01 13:18:14 +0100

    organizing files into folders

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from extract_bike_points import extract_bikes
-from load_bike_points import load_bikes
+from modules.extract_bike_points import extract_bikes
+from modules.load_bike_points import load_bikes
 
 extract_bikes()
 load_bikes()
